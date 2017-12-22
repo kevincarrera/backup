a62b37a3771cfa44e6245e9f28922079ed0a4549
Johann Carrera <xthejohann21x@gmail.com>
2017-12-22 11:42:02 -0500

    script re reinicio

---
+++ opt/reiniciaVpn.sh
@@ -0,0 +1,17 @@
+ruta="/home/maestro/backup/data/temp";
+date=`/bin/date +%Y%m%d`
+echo $ruta/reset_${date}.log;
+if [ -f $ruta/reset_${date}.log  ]; then
+echo "existe";
+
+mv  $ruta/reset_${date}.log $ruta/reset_${date}_ok.log;
+inicio=`date`;
+echo  "se inicio el reinicio $inicio">>${ruta}/reiniciaOpenVpn.log;
+ sudo service openvpn restart>>${ruta}/reiniciaOpenVpn.log;
+fin=`date`;
+echo  "acabo el reinicio $fin">>${ruta}/reiniciaOpenVpn.log;
+
+else
+echo "no existe";
+fi;
+
